04f6f378a74e2b14a9d0664768b8519ea1d1362b
RoxyPursey <roxy.pursey@gmail.com>
2020-04-01 12:24:05 +0100

    added proptypes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,21 @@ import Contact from './components/Contact';
 function App() {
   return (
     <div className="App">
-        <Contact />
+        <Contact 
+         name = "Erika Barnes"
+         avatar = "https://randomuser.me/api/portraits/women/76.jpg"
+         online = "true"
+          />
+           <Contact 
+         name = "Derrick Nichols"
+         avatar = "https://randomuser.me/api/portraits/men/95.jpg"
+         online = "false"
+          />
+           <Contact 
+         name = "Ben Myers"
+         avatar = "https://randomuser.me/api/portraits/men/88.jpg"
+         online = "true"
+          />
     </div>
   );
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import './Contact.css';
 
-const name = "Erika Barnes"
-const avatar = "https://randomuser.me/api/portraits/women/76.jpg"
-const online = "true"
-
-function Contact() {
+function Contact(props) {
     return (
       <div className="Contact">
-      <img className="avatar" src={avatar} alt={name} />
+      <img className="avatar" src={props.avatar} alt={props.name} />
       <div>
-        <h3 className="name">{name}</h3>
-        <div className="status">
-          <span className={online ? "status-online" : "status-offline"}></span>
-          <p className="status-text">{online ? "Online" : "Offline"}</p>
+        <h3 className="name">{props.name}</h3>
+          <div className="status">
+          <div className= {props.online ==="true" ? "status-online" : "status-offline"}> </div>
+          <p className="status-text">{props.online === "true" ? "Online" : "Offline"}</p>
         </div>
       </div>
     </div>
