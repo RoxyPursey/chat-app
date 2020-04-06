496e703c5f77d29c327cd3d3a7de1057c9307df2
RoxyPursey <roxy.pursey@gmail.com>
2020-04-06 14:30:57 +0100

    changing from funtional component to class component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,23 +1,30 @@
 import React from "react";
-import PropTypes from "prop-types";
 import "./Contact.css";
 
-const Contact = ({ name, avatar, status }) => (
-  <div className="Contact">
-    <img className="avatar" src={avatar} alt={name} />
+class Contact extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      online: props.online
+    };
+  }
+  render() {
+    return (
+  <div className="Contact" onClick={event => {
+    const newOnline = !this.state.online;
+    this.setState({ online: newOnline });
+  }} >
+    <img className="avatar" src={this.props.avatar} alt={this.props.avatar} />
     <div>
-      <h3 className="name">{name}</h3>
+      <h3 className="name">{this.props.name}</h3>
       <div className="status">
-        <span className={status ? "status-online" : "status-offline"} />
-        <p className="status-text">{status ? "online" : "offline"}</p>
+        <span className={this.state.online ? "status-online" : "status-offline"} />
+        <p className="status-text">{this.state.online ? "online" : "offline"}</p>
       </div>
     </div>
   </div>
-);
+    );
+  }
+}
 
-Contact.propTypes = {
-  name: PropTypes.string.isRequired,
-  status: PropTypes.bool.isRequired,
-  avatar: PropTypes.string.isRequired
-};
 export default Contact;
